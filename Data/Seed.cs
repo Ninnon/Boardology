70a3ae7c2cec217155bf3df9bbee62828298ad50
Trenton Neilson <TNEILSON@1800contacts.com>
2019-02-28 13:11:03 -0700

    get seeding data ready

---
+++ Data/Seed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Boardology.API.Models;
+using Newtonsoft.Json;
+
+namespace Boardology.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _context;
+
+        public Seed(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void SeedGames()
+        {
+            var gameData = System.IO.File.ReadAllText("Data/GameSeedData.json");
+            var games = JsonConvert.DeserializeObject<List<Game>>(gameData);
+            foreach (var game in games)
+            {
+                _context.Games.Add((game));
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
